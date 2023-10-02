1c5ccd72a4b8d6a29e9d9bba1e61480c0a62f54f
peitonggggg <peitong1121@gmail.com>
2023-10-02 19:42:39 +0800

    Revert "commit"
    This reverts commit 0ae7a4dc0eeb4fd72f19488f58d695312fd688f1, reversing
    changes made to 67da28a6a15b42cd4cc93dfc56081c8e2995afdd.

--- Assignment 3/Assets/scripts/obsticles.cs
+++ Assignment 3/Assets/scripts/obsticles.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class obsticles : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject player;
-    public float moveSpeed;
     void Start()
     {
         
@@ -15,6 +13,6 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed);
+        
     }
 }
