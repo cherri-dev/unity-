ae72c8d0a446c70e6ebd03a944037328f2c53564
cherri-dev <vanessaykw-wk22@student.tarc.edu.my>
2023-10-02 16:18:47 +0800

    initial commit

---
+++ Assignment 3/Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public float moveSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed);
+    }
+}
