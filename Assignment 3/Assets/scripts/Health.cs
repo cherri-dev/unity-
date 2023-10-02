342bddfb4def937f361f98fdd78309770aaae802
cherri-dev <vanessaykw-wk22@student.tarc.edu.my>
2023-10-02 19:43:51 +0800

    Revert "character died"
    This reverts commit c404f0d6065183493a1654e6a72a964730c77062.

--- Assignment 3/Assets/scripts/Health.cs
+++ Assignment 3/Assets/scripts/Health.cs
@@ -6,8 +6,6 @@ public class Health : MonoBehaviour
  [SerializeField] private float startingHealth;
  public float currentHealth{ get; private set; }
  private Animator anim;
-
- public GameObject[] Heart;
  private void Awake()
  {
     currentHealth = startingHealth;
@@ -17,9 +15,6 @@ public class Health : MonoBehaviour
  public void TakeDamage(float _damage)
  {
     currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
-
-    Heart[(int)currentHealth].SetActive(false);
-
     if (currentHealth > 0)
     {
       anim.SetTrigger("hurt");
