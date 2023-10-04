7f152c71cdd32656f4a9ec962667de7e84731b70
cherri-dev <vanessaykw-wk22@student.tarc.edu.my>
2023-10-04 16:11:40 +0800

    .

---
+++ Assignment 3/Assets/scripts/audiomanager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiomanager : MonoBehaviour
+{
+  [SerializeField] AudioSource musicSource;
+  [SerializeField] AudioSource SFXSource;
+
+  public AudioClip background;
+}
